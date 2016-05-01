e57fb612e721ebf6f74cbacedb49ba8cad95bd76
Ernesto De Los Santos <netos_id@yahoo.com>
2016-05-01 17:58:53 -0500

    Multiple changes on css

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,9 @@
 $(function(){
   var $todoInput = $("[data-js='type__here']");
   var $form = $("[data-js='form']");
+  var $listItems = $("[data-js='click__me']");
+
+
   $form.on("submit", function(e){
     e.preventDefault();
   })
