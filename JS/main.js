41810ad8eb05105368bca92e7becc9039c82cdea
Ernesto De Los Santos <netos_id@yahoo.com>
2016-05-02 15:14:47 -0500

    Fixed jquery code.

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,16 @@ $(function(){
   var $todoInput = $("[data-js='type__here']");
   var $form = $("[data-js='form']");
   var newListitem = $("[data-js='input-here']").val();
-    if(newListitem > 0){
-      $("todo-list").append("<li>" + newListitem "</li>");
-      $("#input-here").val('');
-    }
+
 
   $form.on("submit", function(e){
     e.preventDefault();
   })
   $todoInput.on("keyup", function(e){
     console.log(e);
+    if($todoInput.val().length > 1 && e.keyCode === 13){
+      $("[data-js='todo-list']").append("<li>" + $todoInput.val() + "</li>");
+      $todoInput.val('');
+    }
   });
 })
