b5b29982a5d2357ee77adc92dcb71e9ca147163e
Ernesto De Los Santos <netos_id@yahoo.com>
2016-05-03 22:27:40 -0500

    began adding logic to delete item.

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,32 @@
 $(function(){
   var $todoInput = $("[data-js='type__here']");
   var $form = $("[data-js='form']");
-  var newListitem = $("[data-js='input-here']").val();
+  var $todoList = $("[data-js='todo-list']");
 
 
   $form.on("submit", function(e){
     e.preventDefault();
   })
+
   $todoInput.on("keyup", function(e){
     console.log(e);
     if($todoInput.val().length > 1 && e.keyCode === 13){
-      $("[data-js='todo-list']").append("<li>" + $todoInput.val() + "</li>");
+      $todoList.append("<li data-js='todo-item'>" + $todoInput.val() + "</li>");
       $todoInput.val('');
     }
   });
+  $todoList.on("click", "[data-js='todo-item']", function(e){
+    console.log(e.target);
 })
+
+//this to delet a "todo"
+//find todo item that was click
+  $todoList.on("keydown", function(e){
+    if(e.keyCode === 8){
+      $()
+      }
+    });
+})
+})
+//press delete key
+//delete todo
